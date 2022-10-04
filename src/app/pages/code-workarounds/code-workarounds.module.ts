0308e29d9a95546ecc2176d9cb66d562e0f2f36b
Adiel Seffrin <adielseffrin@gmail.com>
2022-10-03 22:23:53 -0300

    fix bundle sizes

--- src/app/pages/code-workarounds/code-workarounds.module.ts
+++ src/app/pages/code-workarounds/code-workarounds.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CodeWorkaroundsRoutingModule } from './code-workarounds-routing.module';
 import { DevexpressGridAdvancedFilterComponent } from './devexpress-grid-advanced-filter/devexpress-grid-advanced-filter.component';
-import { DxButtonModule, DxDataGridModule, DxDateBoxModule, DxTextBoxModule } from 'devextreme-angular';
+import { DxButtonModule } from 'devextreme-angular/ui/button';
+import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
+import { DxDateBoxModule } from 'devextreme-angular/ui/date-box';
+import { DxTextBoxModule  } from 'devextreme-angular/ui/text-box';
 import { EmployeesService } from 'src/app/services/employee.service';
 
 
